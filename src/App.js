a1aa9501bfce47d0e96bb1854c7e781e2637962e
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-10-21 04:25:27 -0700

    db

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 import './App.css';
-import config from '../../config';
+import config from './config';
 import '@progress/kendo-theme-material/dist/all.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
