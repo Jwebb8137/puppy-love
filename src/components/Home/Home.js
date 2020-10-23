c1e42f712d204e1ce2fade6ed36f65d2136c4928
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-10-23 03:00:35 -0700

    css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,20 +19,20 @@ class Home extends React.Component {
           <Link to='signin'><ButtonAlt name='Login' icon='fas fa-sign-in-alt'/></Link>      
         </div>
         <h2 className="mt-5">Pet <span className="pink">Lovers</span> Unite!</h2>
-        <p className="sub-text">A dating app focused on pets? This should have been thought of sooner ...</p>
+        <p className="sub-text mt-1">A dating app focused on pets? This should have been thought of sooner ...</p>
         <div className='container'>
           <img src={Photo1} className='photo-100'/>
           <div>
             <i class="fas fa-paw lg-icon pink"></i>
             <h2 className='sub-heading'>The secret to finding <span className='pink'>LOVE</span>?</h2>
-            <p className='sub-text'>For pet lovers that's easy! Your furry best friend not only hold's our heart but is going to be your key for finding that special someone!</p>
+            <p className='sub-text mt-1'>For pet lovers that's easy! Your furry best friend not only hold's our heart but is going to be your key for finding that special someone!</p>
             <Link to='signup'><Button name='Sign Up Now' icon='fas fa-paw'/></Link>
           </div>
           <img src={Photo2} className='photo-100'/>
           <div>
             <i class="fas fa-heart lg-icon pink"></i>            
             <h2 className='sub-heading'>Matching with a fellow <span className='pink'>PET LOVER</span> just makes sense!</h2>
-            <p className='sub-text'>Knowing how special your furry companion is to you why would you waste time with anyone who wouldn't feel the same?</p>
+            <p className='sub-text mt-1'>Knowing how special your furry companion is to you why would you waste time with anyone who wouldn't feel the same?</p>
             <Link to='browse'><ButtonAlt name='Look Nearby' icon='fas fa-city'/></Link>
           </div>
         </div>
