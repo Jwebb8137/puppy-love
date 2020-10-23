27c7f59e0a1e46e0928c9be917501efe0e0070e9
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-10-22 17:12:11 -0700

    css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -28,7 +28,7 @@ class Home extends React.Component {
           </div>
           <img src={Photo2} className='photo-100'/>
           <div>
-            <i class="fas fa-heart mt-10 lg-icon pink"></i>            
+            <i class="fas fa-heart lg-icon pink"></i>            
             <h2 className='sub-heading'>Matching with a fellow <span className='pink'>PET LOVER</span> just makes sense!</h2>
             <p className='sub-text'>Knowing how special your furry companion is to you why would you waste time with anyone who wouldn't feel the same?</p>
             <Link to='browse'><ButtonAlt name='Look Nearby' icon='fas fa-city'/></Link>
