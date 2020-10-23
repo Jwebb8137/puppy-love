cea215e9d37b43b5f995adee602188c9c67e37be
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-10-22 19:50:41 -0700

    css

--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -32,7 +32,7 @@ const ResultsList = (props) => {
   }
   return (
     <Fragment>
-      <div className="bg-pink">
+      <div className="bg-pink-alt">
         <Carousel />
       </div>
     </Fragment>
