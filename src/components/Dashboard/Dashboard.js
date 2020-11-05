c0132b4c058a3da3c4071116380995c7eb2bf5e3
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-11-04 16:16:14 -0800

    restructure

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -16,7 +16,7 @@ const Dashboard = ({ setAuth }) => {
         method: "GET",
         headers: { token: localStorage.token }
       });
-      const parseRes = await JSON.parse(JSON.stringify(response));
+      const parseRes = await response.json();
       setUserInfo(parseRes)
     } catch (err) {
       console.error(err.message)
